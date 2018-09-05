67d1ee4d5a7d9478e686856d0a91997399893fdf
Rose Parker <parker.rose@ymail.com>
2018-09-05 01:16:43 +0000

    Day 0 -2

---
+++ 30_days.js
@@ -0,0 +1,30 @@
+//DAY 0
+
+ console.log("Hello, World.");
+ 
+//DAY 1
+
+  // Declare second integer, double, and String variables.
+  var secondI;
+  var secondD;
+  var secondStr;
+    // Read and save an integer, double, and String to your variables.
+secondI = parseInt(readLine());
+secondD = parseFloat(readLine());
+secondStr = readLine();
+    // Print the sum of both integer variables on a new line.
+console.log(i + secondI);
+    // Print the sum of the double variables on a new line.
+console.log((d + secondD).toFixed(1));
+    // Concatenate and print the String variables on a new line
+console.log(s + secondStr);
+
+//DAY 2
+
+ var totalCost = Math.round(meal_cost + (meal_cost * tip_percent/100) + (meal_cost * tax_percent/100));
+  
+    console.log("The total meal cost is " + totalCost + " dollars.");
+
+}
+
+
