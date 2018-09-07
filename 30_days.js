04492b455a3a8982f381c01af3b7daca73b00bdf
Rose Parker <parker.rose@ymail.com>
2018-09-07 00:55:17 +0000

    Day 4

--- 30_days.js
+++ 30_days.js
@@ -37,3 +37,27 @@ function main() {
         console.log("Not Weird");
 
 }  
+
+//DAY 4
+function Person(initialAge){
+    // Add some more code to run some checks on initialAge
+        var age = initialAge;
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console
+ if (age < 13 && age > -1){
+     console.log("You are young.");
+ } else if (age > 12 && age < 18) {
+     console.log("You are a teenager.");
+ } else if (age > 17) {
+     console.log("You are old.");
+ } else {
+    console.log("Age is not valid, setting age to 0.");
+                age = 0;
+     this.amIOld();
+ }
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+       age++;
+   };
+}
